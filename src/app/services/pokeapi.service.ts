9c3fe49b9a034c2d187e17fca9a93703bb6576b9
jaro <jaroinz@gmail.com>
2021-09-27 17:32:47 -0300

    adding service for http requests

---
+++ src/app/services/pokeapi.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+/**
+ * handling http requests
+ */
+export class PokeapiService {
+
+  apiURL = 'https://pokeapi.co/api/v2';
+
+  offsetDefault = 20;
+  limit = 20;
+
+  constructor(private http: HttpClient) { }
+
+  listPokemon(next: any, previous: any): Observable<any>
+  {
+    if (next) {
+      return this.http.get(`${next}`);
+    }
+    if (previous) {
+      return this.http.get(`${previous}`);
+    }
+    return this.http.get(`${this.apiURL}/pokemon`);
+  }
+
+  viewPokemon(pokeId: string): Observable<any>
+  {
+    return this.http.get(`${this.apiURL}/pokemon/${pokeId}`);
+  }
+
+  viewSpecies(speciesUrl: string): Observable<any>
+  {
+    return this.http.get(`${speciesUrl}`);
+  }
+
+  viewEvolution(evolutionUrl: string): Observable<any>
+  {
+    return this.http.get(`${evolutionUrl}`);
+  }
+
+}
